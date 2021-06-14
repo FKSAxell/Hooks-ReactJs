c47415446b221f0476f4d765931a95266188fc9c
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-06-14 17:22:58 -0500

    Frase cargada

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -2,11 +2,39 @@ import React from 'react'
 import { useFetch } from '../../hooks/useFetch'
 import "../02-useEffect/effects.css"
 export const MultipleCustomHooks = () => {
-    const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-    console.log(state);
+    const { loading, data } = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
+    // const {state,increment} = useCounter
+    const { author, quote } = !!data && data[0];
+    console.log(data);
+    console.log(author);
+    console.log(quote);
+
     return (
         <div>
-            <h6>MultipleCustomHooks</h6>
+            <h6>The Breaking Bad</h6>
+            <hr />
+
+
+
+            {loading ?
+                <div className="text-center alert alert-info">
+                    Loading...
+                </div>
+                :
+                <blockquote className="text-end blockquote">
+                    <p className="mb-8">{quote}</p>
+                    <footer className="blockquote-footer">
+                        {author}
+                    </footer>
+                </blockquote>
+            }
+            {loading &&
+                <button className="btn btn-primary">
+                    Siguiente Frase
+                </button>
+            }
+
+
         </div>
     )
 }
