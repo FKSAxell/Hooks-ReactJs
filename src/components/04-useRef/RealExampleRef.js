417cd651df61334b2c3e245e6f289842af842a68
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-06-16 23:38:57 -0500

    Update RealExampleRef.js

--- src/components/04-useRef/RealExampleRef.js
+++ src/components/04-useRef/RealExampleRef.js
@@ -1,15 +1,10 @@
-import React from 'react'
-import "../02-useEffect/effects.css"
+import React from "react";
+import "../02-useEffect/effects.css";
 export const RealExampleRef = () => {
-
-    return (
-        <div>
-            <h1>RealExampleRef</h1>
-            <hr/>
-
-            
-
-        </div>
-    );
-
-}
+  return (
+    <div>
+      <h1>RealExampleRef</h1>
+      <hr />
+    </div>
+  );
+};
