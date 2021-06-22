9936e20338b87eda25f115d06458884eefc571d9
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-06-22 00:29:07 -0500

    Layout | Memo

--- src/components/05-useLayoutEffect/Layout.js
+++ src/components/05-useLayoutEffect/Layout.js
@@ -1,28 +1,32 @@
-import React from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import { useCounter } from "../../hooks/useCounter";
 import { useFetch } from "../../hooks/useFetch";
-import "../02-useEffect/effects.css";
+import "./layout.css";
 export const Layout = () => {
   const { counter, increment } = useCounter(1);
   const { data } = useFetch(
     `https://www.breakingbadapi.com/api/quotes/${counter}`
   );
   // const {state,increment} = useCounter
-  const { author, quote } = !!data && data[0];
+  const { quote } = !!data && data[0];
+  const [boxSize, setBoxSize] = useState({});
   console.log(data);
-  console.log(author);
   console.log(quote);
-
+  const pTag = useRef();
+  useLayoutEffect(() => {
+    setBoxSize(pTag.current.getBoundingClientRect());
+  }, [quote]);
   return (
     <div>
       <h6>Layout Effect</h6>
       <hr />
 
       <blockquote className="text-end blockquote">
-        <p className="mb-8">{quote}</p>
-        <footer className="blockquote-footer">{author}</footer>
+        <p className="mb-8" ref={pTag}>
+          {quote}
+        </p>
       </blockquote>
-
+      <pre>{JSON.stringify(boxSize, null, 3)}</pre>
       <button className="btn btn-primary" onClick={increment}>
         Siguiente Frase
       </button>
