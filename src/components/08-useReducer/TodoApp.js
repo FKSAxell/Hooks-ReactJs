7c8526b023f4336a5bc8eb38a469c8d7bb1ccb60
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-06-23 16:38:16 -0500

    todo Toggle

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -28,6 +28,10 @@ export const TodoApp = () => {
         dispatch(action);
     }
 
+    const handleToggle = (todoId) => {
+        dispatch({ type: "toggle", payload: todoId });
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
         if (description.trim().length <= 1) {
@@ -63,7 +67,7 @@ export const TodoApp = () => {
                                         key={todo.id}
                                         className="list-group-item"
                                     >
-                                        <p className="text-center ">{i + 1}. {todo.desc}</p>
+                                        <p className={`${todo.done && "complete"}`} onClick={() => handleToggle(todo.id)}>{i + 1}. {todo.desc}</p>
                                         <button className="btn btn-danger" onClick={() => handleDelete(todo.id)}>
                                             Borrar
                                         </button>
