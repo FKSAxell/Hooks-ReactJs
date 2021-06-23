81d9bffb581d841613cd118787e906433316788d
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-06-23 12:03:15 -0500

    Fin Intro Hook

---
+++ src/components/06-memos/CallBack.js
@@ -0,0 +1,25 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import "../02-useEffect/effects.css";
+import { ShowIncrement } from './ShowIncrement';
+export const CallBack = () => {
+    const [counter, setCounter] = useState(10);
+    // const increment =()=>{
+    //     setCounter(counter+1);
+    // }
+    const increment = useCallback(
+        (num) => {
+            setCounter(c=>c+num);
+        },
+        [setCounter],
+    )
+    useEffect(() => {
+       //??
+    }, [increment])
+    return (
+        <div>
+            <h1>Use CallBack Hook: {counter}</h1>
+            <hr/>
+            <ShowIncrement increment={increment}/>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { MemoHook } from "./components/06-memos/MemoHook";
+// import { CallBack } from "./components/06-memos/CallBack";
+import { Padre } from "./components/07-tarea-memo/Padre";
+// import { MemoHook } from "./components/06-memos/MemoHook";
 // import { CounterApp } from "./components/01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 // import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
@@ -8,11 +10,11 @@ import { MemoHook } from "./components/06-memos/MemoHook";
 // import { FocusScreen } from "./components/04-useRef/FocusScreen";
 // import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
 // import { Layout } from "./components/05-useLayoutEffect/Layout";
-import { Memorize } from "./components/06-memos/Memorize";
+// import { Memorize } from "./components/06-memos/Memorize";
 // import { SimpleForm } from "./components/02-useEffect/SimpleForm";
 // import { HookApp } from "./HookApp";
 
-ReactDOM.render(<MemoHook />, document.getElementById("root"));
+ReactDOM.render(<Padre />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
