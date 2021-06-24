1824556a04bd91e78753f394729aa50a740071d7
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-06-24 17:00:21 -0500

    NavBar

--- src/components/09-useContext/AppRouter.js
+++ src/components/09-useContext/AppRouter.js
@@ -2,20 +2,26 @@ import React from 'react'
 import {
     BrowserRouter as Router,
     Switch,
-    Route
+    Route,
+    Redirect
 } from "react-router-dom";
 import { AboutScreen } from './AboutScreen';
 import { HomeScreen } from './HomeScreen';
 import { LoginScreen } from './LoginScreen';
+import { NavBar } from './NavBar';
 
 export const AppRouter = () => {
+
     return (
         <Router>
             <div>
+                <NavBar />
                 <Switch>
                     <Route exact path="/" component={HomeScreen} />
                     <Route exact path="/login" component={LoginScreen} />
                     <Route exact path="/about" component={AboutScreen} />
+                    <Redirect to="/" />
+                    {/* <Route component={HomeScreen} /> */}
                 </Switch>
             </div>
         </Router>
